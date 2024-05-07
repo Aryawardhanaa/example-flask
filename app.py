5b8935d5c041af2367574b50ac00a0f75da23150
baploarya <aryawardhana7831@gmail.com>
2024-05-08 00:59:47 +0700

    lagi

--- app.py
+++ app.py
@@ -6,12 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    return 'Hello from Koyeb'
+
+
+@app.route('/test')
+def hello_world():
     response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[{"role": "user", "content": "Hello"}],
 )
     return response.choices[0].message.content
-
-
 if __name__ == "__main__":
     app.run()
