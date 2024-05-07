4804834962704ea510b144061d6d9e523a46357f
baploarya <aryawardhana7831@gmail.com>
2024-05-08 01:10:32 +0700

    teslagi

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Koyeb'
+    return 'Hello from Koye'
 
 
 @app.route('/test')
 def hello_world():
     response = client.chat.completions.create(
     model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "Hello"}],
+    messages=[{"role": "user", "content": "Hello"}]
 )
     return response.choices[0].message.content
 if __name__ == "__main__":
