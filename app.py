3e688b62b74a8aadb2a973d8a5f73058e50bdfb0
baploarya <aryawardhana7831@gmail.com>
2024-05-08 00:54:06 +0700

    test

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from g4f.client import Client
+
+client = Client()
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Koyeb'
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Hello"}],
+)
+    return response.choices[0].message.content
 
 
 if __name__ == "__main__":
